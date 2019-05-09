4db9e80976081b5e68540603acdab85e1cccaa6a
Tasos Kyr <tasoskyr1@gmail.com>
2019-05-09 14:23:12 +0200

    Auth setup v.1

--- server/app.js
+++ server/app.js
@@ -9,7 +9,10 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
 
+require("./configs/passport");
 
 mongoose
   .connect('mongodb://localhost/server', { useNewUrlParser: true })
@@ -27,7 +30,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5000",
+    origin: "http://localhost:3000",
     credentials: true
   })
 );
@@ -52,18 +55,27 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.use(
+  session({
+    secret: "this is a secret key",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
 app.use('/', index);
 
+const auth = require("./routes/auth/auth");
+app.use("/api", auth);
+
 const events = require("./routes/events");
 app.use("/", events);
-
 
 module.exports = app;
