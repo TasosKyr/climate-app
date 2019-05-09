d089c513bab5ccab6f86ca3e1f7a3f84c02a400a
amrwski <ant.morawski@gmail.com>
2019-05-09 12:34:40 +0200

    basic navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import Navbar from "./components/Navbar"
 import Events from "./components/Action/Events"
 
 import "./App.css"
@@ -10,14 +10,14 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {/* <Navbar /> */}
-        <Switch>
-          {/* <Route path="/" component={Home} /> */}
-          {/* <Route path="/politics" component={Politics} /> */}
-          {/* <Route path="/action" component={Action} /> */}
-          {/* <Route path="/data" component={Data} /> */}
-          <Events />
-        </Switch>
+        <Navbar />
+        {/* <Switch> */}
+        {/* <Route path="/" component={Home} /> */}
+        {/* <Route path="/politics" component={Politics} /> */}
+        {/* <Route path="/action" component={Action} /> */}
+        {/* <Route path="/data" component={Data} /> */}
+        <Events />
+        {/* </Switch> */}
         {/* <Footer /> */}
       </div>
     )
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+class Navbar extends React.Component {
+  state = {}
+  render() {
+    return (
+      <nav class="nav-bar">
+        <Link to="/politics" style={{}}>
+          Politics
+        </Link>
+        <Link to="/action" style={{}}>
+          Action
+        </Link>
+        <Link to="/data" style={{}}>
+          Data
+        </Link>
+        <Link to="/signup" style={{}}>
+          Signup
+        </Link>
+        <Link to="/login" style={{}}>
+          Login
+        </Link>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
