10d80e19f31e8683191b07818be698dadb4b7461
amrwski <ant.morawski@gmail.com>
2019-05-09 11:39:28 +0200

    added routes in app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from "react"
+
+import Events from "./components/Action/Events"
+
 import "./App.css"
-import Events from './components/Action/Events'
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {}
 
-      <Events />
-    </div>
-
-  )
+  render() {
+    return (
+      <div className="App">
+        {/* <Navbar /> */}
+        <Switch>
+          {/* <Route path="/" component={Home} /> */}
+          {/* <Route path="/politics" component={Politics} /> */}
+          {/* <Route path="/action" component={Action} /> */}
+          {/* <Route path="/data" component={Data} /> */}
+          <Events />
+        </Switch>
+        {/* <Footer /> */}
+      </div>
+    )
+  }
 }
 
 export default App
