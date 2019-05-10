c9bbf662038785a2c3098733fa226dc02a78d6af
amrwski <ant.morawski@gmail.com>
2019-05-10 11:29:20 +0200

    merge fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import React from "react"
 
 import Home from "./components/Home/Home"
-import { Switch, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar"
-import Signup from "./components/Auth/Signup";
-import Login from "./components/Auth/Login";
-import { loggedin } from "./services/auth";
+import Signup from "./components/Auth/Signup"
+import Login from "./components/Auth/Login"
+import { loggedin } from "./services/auth"
 import Events from "./components/Action/Events"
 import Footer from "./components/Footer"
 
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./components/ProtectedRoute"
 import Profile from "./components/Profile"
 
-import { Switch, Route, Redirect } from "react-router-dom";
-import Action from './components/Action/Action'
-
+import { Switch, Route, Redirect } from "react-router-dom"
+import Action from "./components/Action/Action"
 
 import "./App.css"
 import "./customBootstrap.scss"
@@ -25,40 +24,29 @@ class App extends React.Component {
   setUser = user => {
     this.setState({
       loggedIn: user
-    });
-  };
+    })
+  }
 
   getUser = () => {
     loggedin().then(user => {
       this.setState({
         loggedIn: user
-      });
-    });
-  };
+      })
+    })
+  }
 
   componentDidMount() {
-    this.getUser();
+    this.getUser()
   }
 
   render() {
     return (
       <div className="App">
- 
         <Navbar setUser={this.setUser} loggedIn={this.state.loggedIn} />
         <Switch>
-          <Route
-            exact
-            path="/signup"
-            render={(props) => <Signup setUser={this.setUser} {...props} />}
-          />
+          <Route exact path="/signup" render={props => <Signup setUser={this.setUser} {...props} />} />
 
-          <Route
-            exact
-            path="/login"
-            render={(props) => <Login
-              setUser={this.setUser}
-              {...props} />}
-          />
+          <Route exact path="/login" render={props => <Login setUser={this.setUser} {...props} />} />
           {/* <Route
             exact
             path="/profile"
@@ -66,12 +54,7 @@ class App extends React.Component {
               <Profile {...props} user={this.state.loggedIn} />
             )}
           /> */}
-          <ProtectedRoute
-            user={this.state.loggedIn}
-            component={Profile}
-            exact={true}
-            path="/profile"
-          />
+          <ProtectedRoute user={this.state.loggedIn} component={Profile} exact={true} path="/profile" />
           {/* <Route path="/" component={Home} /> */}
           {/* <Route path="/politics" component={Politics} /> */}
           {/* <Route path="/action" component={Action} /> */}
@@ -89,11 +72,11 @@ class App extends React.Component {
         {/* <Events /> */}
         {/* </Switch> */}
         <Footer />
-      </div>
+
         {/* </Switch> */}
 
         {/* <Footer /> */}
-      </div >
+      </div>
     )
   }
 }
