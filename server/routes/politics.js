79929df82eb921e71cbcf7ffabf2dcbacbea5ea8
amrwski <ant.morawski@gmail.com>
2019-05-12 18:25:35 +0200

    embedded tweets on fe

--- server/routes/politics.js
+++ server/routes/politics.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const Twitter = require("twit")
 
 router.get("/politics", (req, res) => {
-  console.log("hello from poli")
   let client = new Twitter({
     consumer_key: process.env.TWITTER_API_KEY,
     consumer_secret: process.env.TWITTER_API_KEY_SECRET,
@@ -14,10 +13,9 @@ router.get("/politics", (req, res) => {
   client
     .get("search/tweets", { q: "climatechange" })
     .then(response => {
-      // console.log(response.data)
       res.json(response.data)
     })
-    .catch(err => console.log("Something went wrong!", err))
+    .catch(err => console.error(err))
 })
 
 module.exports = router
