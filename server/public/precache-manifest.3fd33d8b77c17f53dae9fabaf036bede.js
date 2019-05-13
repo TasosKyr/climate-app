81aba4b25fd39e125e2d7264d4fbcbc029a69c64
amrwski <ant.morawski@gmail.com>
2019-05-13 12:09:53 +0200

    first deploy

---
+++ server/public/precache-manifest.3fd33d8b77c17f53dae9fabaf036bede.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb3903eec474559c4424499a3d522ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dda92f2db29f1cf270d5",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "5cd85a458b034d62635c",
+    "url": "/static/css/main.60c2ce6e.chunk.css"
+  },
+  {
+    "revision": "dda92f2db29f1cf270d5",
+    "url": "/static/js/2.4a2a91a1.chunk.js"
+  },
+  {
+    "revision": "5cd85a458b034d62635c",
+    "url": "/static/js/main.e91b7d92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "806e5cecb3f780480762e6ac79dbb1b7",
+    "url": "/static/media/601229-unsplash.806e5cec.jpg"
+  },
+  {
+    "revision": "28f6f8199909f1f72f1d0ad2b213efe4",
+    "url": "/static/media/action.28f6f819.jpeg"
+  },
+  {
+    "revision": "7c6c0b9fa90d2c8cd0e9c15a39653c1c",
+    "url": "/static/media/data.7c6c0b9f.jpeg"
+  },
+  {
+    "revision": "177c37e0a0379c5b8532f9eed479593d",
+    "url": "/static/media/politics.177c37e0.jpeg"
+  }
+]);
