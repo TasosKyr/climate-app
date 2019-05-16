7b3ba597167970136d059d5dc4c06b583b5f0824
Helleke Heikkine <helleke.heikkinen@gmail.com>
2019-05-16 09:31:23 +0200

    small fixes on about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,6 @@ export default class About extends Component {
     return (
       <div className="container page-container" id='container'>
         <h1>ClimAct</h1>
-        <h4>Climate change is the most important global political topic at the moment. Many people are aware that we need to make big personal and societal changes in order to tackle this problem. On a personal level many of us struggle to find the ways to do more than just abandon the use of plastic straws (we don’t dispute that they should be abandoned). We acknowledge that the changes needed to fight climate change have to be on a systemic level – this means political changes on a national as well as a global level. </h4>
         <hr />
         <h2>The Problem We Want to Solve</h2>
         <p>Climate change is an imminent threat to the whole world, but what can I do as one person, except from changing my own consumption behavior? I want to change the whole system! </p>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -14,7 +14,7 @@ class Home extends Component {
 
           <div className='intro-text-container'>
             <p>Welcome to ClimAct – This is a page where we want to help You get more engaged in fighting climate change.
-              We have created three categories for you to explore – Politics, Action & Data.
+            Climate change is the most important global political topic at the moment. Many people are aware that we need to make big personal and societal changes in order to tackle this problem. On a personal level many of us struggle to find the ways to do more than just abandon the use of plastic straws (we don’t dispute that they should be abandoned). We acknowledge that the changes needed to fight climate change have to be on a systemic level – this means political changes on a national as well as a global level.
               On the politics section you will find relevant climate change legislation. ON the action page you can find ways to get more engaged in your community or politically. The data page is there for you to explore and understand climate change better.</p>
           </div>
           <hr />
